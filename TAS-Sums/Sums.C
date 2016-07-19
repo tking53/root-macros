12d2ead575c03d2be3075dba122d3dfacef65196
T. T. King <tking36@vols.utk.edu>
2016-07-19 11:33:21 -0400

    new style for gamma gamma plots. tweaks to chainloader and Sums.C

--- TAS-Sums/Sums.C
+++ TAS-Sums/Sums.C
@@ -17,14 +17,14 @@
 #include <string>
 
 
-void Sums(const char* savename, int batchmode=1){
+void Sums(const char* savename,unsigned batchmode=0){
   
   if (batchmode ==1){
-      gROOT->SetBatch(kTRUE);
-  }
+    gROOT->SetBatch(kTRUE); //set batch mode to suppress canvas output
+     }
   //TChain* V;
   // gROOT->ProcessLine(".x dual-seg-chainload.C");
-  TChain *V = new TChain("Tab");
+  TChain *P = new TChain("Tab");
  
    for (int i=1;i<11;i++){
        stringstream ss;
@@ -34,26 +34,31 @@ void Sums(const char* savename, int batchmode=1){
        else if (i >= 10){
   	ss<<"094rb_14-seg-"<<i<<".root";
        }
-       V->Add(ss.str().c_str());
+       P->Add(ss.str().c_str());
    }
   
 
-
+   cout<<"chain loaded"<<endl;
       
   TFile *f = new TFile(savename,"RECREATE");
+
   
   TH2D* NvG = new TH2D("NvG"," NaI (x) vs Ge (y) Paddbacks",8000.,0.,8000.,8000.,0.,8000.);
-  TH2D* LvG = new TH2D("HvG"," LaBr3 (x) vs Ge (y) Paddbacks",8000.,0.,8000.,8000.,0.,8000.);
+  TH2D* LvG = new TH2D("LvG"," LaBr3 (x) vs Ge (y) Paddbacks",8000.,0.,8000.,8000.,0.,8000.);
   TH2D* NvL = new TH2D("NvL"," NaI (x) vs LaBr3 (y) Paddbacks",8000.,0.,8000.,8000.,0.,8000.);
 
+  cout<<"Hist + files made"<<endl;
+    
   NvG->SetMinimum(1);
   LvG->SetMinimum(1);
   NvL->SetMinimum(1);
-
-  V->Draw("pab.GabE:pab.NabE>>NvG");
-  V->Draw("pab.GabE:pab.LabE>>LvG");
-  V->Draw("pab.LabE:pab.NabE>>NvL");
-
+  cout<<"minimum set"<<endl;
+  P->Draw("pab.GabE:pab.NabE>>NvG");
+  cout<<"NvG done"<<endl; 
+  P->Draw("pab.GabE:pab.LabE>>LvG");
+  cout<<"LvG done"<<endl; 
+  P->Draw("pab.LabE:pab.NabE>>NvL");
+  
   f->Write();
 }
   
--- dual-seg-chainload.C
+++ dual-seg-chainload.C
@@ -14,10 +14,10 @@
   for (int i=1;i<11;i++){
       stringstream ss;
       if (i <10){
-	ss<<"094rb_14-seg-0"<<i<<".root";
+	ss<<"/home/tking/segment-94/094rb_14-seg-0"<<i<<".root";
       }
       else if (i >= 10){
-	ss<<"094rb_14-seg-"<<i<<".root";
+	ss<<"/home/tking/segment-94/094rb_14-seg-"<<i<<".root";
       }
       
       C->Add(ss.str().c_str());
@@ -28,17 +28,18 @@
   for (int i=1;i<11;i++){
       stringstream ss;
       if (i <10){
-	ss<<"094rb_14-seg-0"<<i<<".root";
+	ss<<"/home/tking/segment-94/094rb_14-seg-0"<<i<<".root";
       }
       else if (i >= 10){
-	ss<<"094rb_14-seg-"<<i<<".root";
+	ss<<"/home/tking/segment-94/094rb_14-seg-"<<i<<".root";
       }
       
       V->Add(ss.str().c_str());
   }
 
+  //  C->Print();
   V->Print();
-  C->Print();
+ 
 }
 
 
