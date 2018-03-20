c4bfc74d262bed3ac060a42e04712af424353e03
T.T. King <tking.91@gmail.com>
2018-03-20 08:44:42 -0400

    updating the GSaddback class to bring it inline with the one in PAASS

--- GamScint/addbackCalculator/GSaddback.hpp
+++ GamScint/addbackCalculator/GSaddback.hpp
@@ -3,25 +3,32 @@
 
 class GSAddback {
 public:
-  /** Default constructor setting things to zero */
-  GSAddback() {
-    energy = time = multiplicity = 0;
-  }
+    /** Default constructor setting things to zero */
+    GSAddback() {
+        energy = time = multiplicity = abevtnum=ftime = 0;
+    }
+    /** Default deconstructor */
 
-  /** Default constructor setting default values
-   * \param [in] ienergy : the initial energy
-   * \param [in] itime : the initial time
-   * \param [in] imultiplicity : multiplicity of the event */
-  GSAddback(Double_t ienergy, Double_t itime, unsigned imultiplicity) {
-    energy = ienergy;
-    time = itime;
-    multiplicity = imultiplicity;
-  }
+    ~GSAddback(){};
 
-  Double_t energy;//!< Energy of the addback event
-  Double_t time;//!< time of the addback event
-  unsigned multiplicity;//!< multiplicity of the event
+    /** Default constructor setting default values
+     * \param [in] ienergy : the initial energy
+     * \param [in] itime : the initial time
+     * \param [in] imultiplicity : multiplicity of the event
+     * \param [in] iEvtNum: Pixie event number of the event (comes from DetectorDriver)*/
+    GSAddback(double ienergy, double itime, double iftime, unsigned imultiplicity, unsigned long iEvtNum) {
+        energy = ienergy;
+        time = itime;
+        multiplicity = imultiplicity;
+        abevtnum= iEvtNum;
+        ftime = iftime;
+    }
+
+    double energy;//!< Energy of the addback event
+    double time;//!< time of the addback event
+    unsigned multiplicity;//!< multiplicity of the event
+    unsigned long abevtnum;//!<pixie event number of the last det event added to the addback calc
+    double ftime;//<! first time in the event
 };
-
 
 #endif //end include guard
