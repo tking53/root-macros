f89563344ac1d4017d44d9b43606637209fa2797
T.T. King <tking.91@gmail.com>
2018-03-20 08:41:27 -0400

    updates to the Tchain Loader

--- GamScint/loadGStchain.cxx
+++ GamScint/loadGStchain.cxx
@@ -5,14 +5,19 @@
 #include "TSystem.h"
 #include "TObject.h"
 #include "TROOT.h"
-//#include "TProof.h"
+#include "TProof.h"
 
 
 TChain *chain1;
 TChain *chain2;
 Bool_t abfile;
 
+TProof *pr;
+
 int loadGStchain(const std::string paassFile="sing"){
+
+  pr = TProof::Open("lite://","workers=8");
+
   if (paassFile == "ab"){
     chain1 = new TChain("GSsinglesAB");
     abfile = true;
@@ -26,11 +31,11 @@ int loadGStchain(const std::string paassFile="sing"){
     for (auto it=1; it<=5 ; it++){
       if (abfile){
         std::stringstream ss;
-        ss <<"/home/hanayo/research/ornl2016/thesis/rootFiles/94rbSets/94rb_14_set"<<it<<"/094rb_14_5scan_set"<<it<<"-GammaScintPAB.root";
+        ss <<"/home/hanayo/research/ornl2016/thesis/interHolding/mk2_94rb/94rb_14_set"<<it<<"/094rb_14_5scan_set"<<it<<"-GammaScintPAB.root";
         chain1->Add(ss.str().c_str());
       }else{
         std::stringstream ss;
-        ss << "/home/hanayo/research/ornl2016/thesis/rootFiles/94rbSets/94rb_14_set"<<it<<"/094rb_14_5scan_set"<<it<<"-GammaScint.root";
+        ss << "/home/hanayo/research/ornl2016/thesis/interHolding/mk2_94rb/94rb_14_set"<<it<<"/094rb_14_5scan_set"<<it<<"-GammaScint.root";
         chain1->Add(ss.str().c_str());
         chain2->Add(ss.str().c_str());
       }
@@ -41,7 +46,19 @@ int loadGStchain(const std::string paassFile="sing"){
     }else {
       gDirectory->Add(chain1);
       gDirectory->Add(chain2);
+      chain1->AddFriend(chain2);
     }
+
+    if (abfile){
+      chain1->SetProof();
+      std::cout << "Number of TTrees added to chain1: " << chain1->GetNtrees() << std::endl;
+    } else {
+      chain1->SetProof();
+      chain2->SetProof();
+      std::cout << "Number of TTrees added to chain1: " << chain1->GetNtrees() << std::endl;
+      std::cout << "Number of TTrees added to chain2: " << chain2->GetNtrees() << std::endl;
+    }
+
     //  chain->Add("/home/hanayo/research/ornl2016/thesis/rootFiles/94rbSets/94rb_14_set1/094rb_14_5scan_set1-GammaScintPAB.root");
 
 
