bb6f13b8eff982534489da2a9c5c837fa8617e93
T.T. King <tking.91@gmail.com>
2018-03-20 08:49:38 -0400

    old SinglesPlotter.C

--- GamScint/totalsPlotter/totalsPlotter2.cxx
+++ GamScint/totalsPlotter/totalsPlotter2.cxx
@@ -9,6 +9,10 @@
 #include "TStyle.h"
 #include "TProfile.h"
 #include "TChain.h"
+#include "TTreeReader.h"
+#include "TTreeReader.h"
+#include "TTreeReaderArray.h"
+#include "TTreeReaderValue.h"
 
 #include <fstream>
 #include <iostream>
@@ -17,87 +21,77 @@
 #include <cmath>
 #include <string>
 
-//void totalsPlot(const char* inputname, const char* savename, const bool totalsOnly = false,const bool batchMode=false){
 void totalsPlotter2(const char* savename,const bool batchMode=true,  const bool totalsOnly = false){
   //using namespace std;
   if (batchMode){
     gROOT->SetBatch(kTRUE);
   }
 
-  
-  //TFile  *f = TFile::Open(inputname);
+  TTreeReader SingReader;
+  TTreeReader ABReader;
 
-  //  std::string saveFile = savename =".root";
-  //  TFile fnew(saveFillayerse,"RECREATE");
   TFile fnew(savename,"RECREATE");
 
-  TChain *Tsing = (TChain*)gROOT->FindObject("GSsingles");
+  TChain *GS = (TChain*)gROOT->FindObject("GSsingles");
+  TChain *AB = (TChain*)gROOT->FindObject("GSaddback");
 
-  //TTree *Tsing = (TTree*)f->Get("GSsingles");
-  
   TH1D* NaITotal = new TH1D("NaITotal","NaI Energy (Totals)",8000.,0.,8000.);
   TH1D* NaITotalBG = new TH1D("NaITotalBG","NaI Energy Beta Gate (Totals)",8000.,0.,8000.);
+  TH1D* NaITWAB  = new TH1D("NaITypeWideAB","NaI AddBack Energy (TypeWide)",8000.,0.,8000.);
+  TH1D* NaITWABBG  = new TH1D("NaITypeWideABBG","NaI BG AddBack Energy (TypeWide)",8000.,0.,8000.);
 
   TH1D* SHTotal = new TH1D("SHTotal"," HAGRiD Energy (Totals)",8000.,0.,8000.);
-  TH1D* SHTotalBG = new TH1D("SHTotalBG"," HAGRiD Energy Beta Gate (Totals)",8000.,0.,8000.);
-  
-  Tsing->Draw("GS_Energy>>SHTotal","GS_Type==\"smallhag\"","");
-  Tsing->Draw("GS_Energy>>SHTotalBG","GS_Type==\"smallhag\" && GS_HasLowResBeta==1","");
-  
-  Tsing->Draw("GS_Energy>>NaITotal","GS_Type==\"nai\"","");
-  Tsing->Draw("GS_Energy>>NaITotalBG","GS_Type==\"nai\" && GS_HasLowResBeta==1","");
-  
-  if (!totalsOnly){
+  TH1D* SHTotalBG = new TH1D("SHTotalBG","HAGRiD Energy Beta Gate (Totals)",8000.,0.,8000.);
+  TH1D* SHTWAB = new TH1D("SHTypeWideAB","HAGRiD AddBack Energy (TypeWide)",8000.,0.,8000.);
+  TH1D* SHTWABBG = new TH1D("SHTypeWideABBG","HAGRiD BG AddBack Energy (TypeWide)",8000.,0.,8000.);
 
-    TH2D* longTimePlotSH = new TH2D("longTimePlotSH"," HAGRiD Long Time Plot (Energy vs BunchNum)",8000.,0.,8000.,2000.,0.,2000.);
-    TH2D* longTimePlotNai = new TH2D("longTimePlotNai"," NaI Long Time Plot (Energy vs BunchNum)",8000.,0.,8000.,2000.,0.,2000.);
+  TH1D* ClovTotal = new TH1D("ClovTotal","HPGe Clover Energy (Totals)",8000.,0.,8000.);
+  TH1D* ClovTotalBG = new TH1D("ClovTotalBG","HPGe Clover Energy Beta Gate (Totals)",8000.,0.,8000.);
+  TH1D* ClovTWAB = new TH1D("ClovTypeWideAB","HPGe Clover AddBack Energy (TypeWide)",8000.,0.,8000.);
+  TH1D* ClovTWABBG = new TH1D("ClovTypeWideABBG","HPGe Clover BG AddBack Energy (TypeWide)",8000.,0.,8000.);
 
-    Tsing->Draw("GS_BunchNum:GS_Energy>>longTimePlotSH","GS_Type==\"smallhag\"");
-    Tsing->Draw("GS_BunchNum:GS_Energy>>longTimePlotNai","GS_Type==\"nai\"");
+  TH1D* SHin[16]={NULL};
+  TH1D* NaiIn[10]={NULL};
+  TH1D* HPGen[4]={NULL};
 
-    TH1D *SHin[16]={NULL};
-    TH1D *NaiIn[10]={NULL};
-
-    for (auto it = 0; it<16;it++){
-      std::stringstream ss;
-      std::stringstream sss;
-      string num = std::to_string(it);
-      ss<<"SH"<<num;
-      sss<<"Small Hagrid #"<<num<<" Energy";
-      SHin[it] = new TH1D(ss.str().c_str(),sss.str().c_str(),8000.,0.,8000.);
-    }
-
-    for (auto it = 0; it<10;it++){
-      std::stringstream ss;
-      std::stringstream sss;
-      string num = std::to_string(it);
-      ss<<"NaI"<<num;
-      sss<<"NaI #"<<num<<" Energy";
-      NaiIn[it] = new TH1D(ss.str().c_str(),sss.str().c_str(),8000.,0.,8000.);
-    }
-
-    for (int it=0; it<16; it++){
-      TH1D* tmp = (TH1D*) SHin[it];
-      std::stringstream cut;
-      cut<<"GS_Type==\"smallhag\" && GS_Num=="<< it;
-      stringstream plotVar;
-      plotVar<<"GS_Energy>>"<<"SH"<<it;
-      Tsing->Draw(plotVar.str().c_str(),cut.str().c_str() ,"");
-      SHin[it]=tmp;
-
-    }
-
-    for (int it=0; it <10; it++){
-      TH1D* tmp = (TH1D*) NaiIn[it];
-      std::stringstream cut;
-      cut<<"GS_Type==\"nai\" && GS_Num=="<< it;
-      stringstream plotVar;
-      plotVar<<"GS_Energy>>"<<"NaI"<<it;
-      Tsing->Draw(plotVar.str().c_str(),cut.str().c_str() ,"");
-      NaiIn[it]=tmp;
-    }
+  for (auto it = 0; it<16;it++){
+    std::stringstream ss;
+    std::stringstream sss;
+    string num = std::to_string(it);
+    ss<<"SH"<<num;
+    sss<<"Small Hagrid #"<<num<<" Energy";
+    SHin[it] = new TH1D(ss.str().c_str(),sss.str().c_str(),8000.,0.,8000.);
   }
-  
+
+  for (auto it = 0; it<10;it++){
+    std::stringstream ss;
+    std::stringstream sss;
+    string num = std::to_string(it);
+    ss<<"NaI"<<num;
+    sss<<"NaI #"<<num<<" Energy";
+    NaiIn[it] = new TH1D(ss.str().c_str(),sss.str().c_str(),8000.,0.,8000.);
+  }
+  for (auto it = 0; it<4;it++){
+    std::stringstream ss;
+    std::stringstream sss;
+    string num = std::to_string(it);
+    ss<<"Clover"<<num;
+    sss<<"Leaf #"<<num<<" Energy";
+    HPGen[it] = new TH1D(ss.str().c_str(),sss.str().c_str(),8000.,0.,8000.);
+  }
+  SingReader.SetTree( GSsingles );
+  ABReader.SetTree( GSaddback );
+
+  while (SingReader.Next()){
+
+
+
+
+
+  }//end singles tree while
+
+
+
   fnew.Write();
   fnew.Close();
   //f->Close();
