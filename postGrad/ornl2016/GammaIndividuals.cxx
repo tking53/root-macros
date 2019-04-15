87eca6fcd6b4276ff6e2e699ceeff100e8a9b29e
T. T. King <tking.91@gmail.com>
2019-04-15 17:00:44 -0400

    GammaIndividuals update
    Now can take a vector of TFile pointers to plot multiple files at once

--- postGrad/ornl2016/GammaIndividuals.cxx
+++ postGrad/ornl2016/GammaIndividuals.cxx
@@ -12,7 +12,7 @@
 
 using namespace std;
 enum EColor kDarkCyan = (enum EColor)(kCyan + 2);
-vector<enum EColor> geCols = {kRed, kGreen, kBlue, kDarkCyan};
+vector<enum EColor> geCols = {kBlack, kRed, kGreen, kBlue};
 
 vector<enum EColor> hagCols = {
     kBlue, (enum EColor)(kBlue + 2),
@@ -20,36 +20,45 @@ vector<enum EColor> hagCols = {
     (enum EColor)(kMagenta + 3), (enum EColor)(kMagenta - 2),
     kGreen, (enum EColor)(kGreen + 2), (enum EColor)(kGreen + 3), (enum EColor)(kGreen + 4), (enum EColor)(kGreen - 2), (enum EColor)(kGreen - 6)};
 
-void CloverIndiviuals(string prefix = "g_E_", Int_t lowR = 0, Int_t highR = 8000) {
+void CloverIndiviuals(vector<TFile*> fileList, string prefix = "g_E_", Int_t lowR = 0, Int_t highR = 8000) {
     auto c1 = new TCanvas("c1", "c1", 800, 600);
+    if (prefix == "") {
+        prefix = "g_E_";
+    }
     stringstream ss;
-    for (Int_t i = 0; i < 4; i++) {
-        stringstream ss;
-        ss << prefix.c_str() << i;
-        TH2* hh = ((TH2*)gFile->Get(ss.str().c_str()));
-        hh->SetLineColor(geCols.at(i));
-        hh->GetXaxis()->SetRangeUser(lowR, highR);
-        if (i == 0) {
-            hh->Draw("");
-        } else {
-            hh->Draw("same");
+    for (Int_t i = 0; i < (Int_t)fileList.size(); i++) {
+        for (Int_t j = 0; j < 4; j++) {
+            stringstream ss;
+            ss << prefix.c_str() << j;
+            TH2* hh = ((TH2*)fileList.at(i)->Get(ss.str().c_str()));
+            hh->SetLineColor(geCols.at(j));
+            hh->GetXaxis()->SetRangeUser(lowR, highR);
+            if (j == 0 && i == 0) {
+                hh->Draw("");
+            } else {
+                hh->Draw("same");
+            }
         }
     }
 }
-
-void HagIndiviuals(string prefix = "h_E_", Int_t lowR = 0, Int_t highR = 8000) {
+void HagIndiviuals(vector<TFile*> fileList, string prefix = "h_E_", Int_t lowR = 0, Int_t highR = 8000) {
     auto c2 = new TCanvas("c2", "c2", 800, 600);
+    if (prefix == "") {
+        prefix = "h_E_";
+    }
     stringstream ss;
-    for (Int_t i = 0; i < 16; i++) {
-        stringstream ss;
-        ss << prefix.c_str() << i;
-        TH2* hh = ((TH2*)gFile->Get(ss.str().c_str()));
-        hh->SetLineColor(hagCols.at(i));
-        hh->GetXaxis()->SetRangeUser(lowR, highR);
-        if (i == 0) {
-            hh->Draw("");
-        } else {
-            hh->Draw("same");
+    for (Int_t i = 0; i < (Int_t)fileList.size(); i++) {
+        for (Int_t j = 0; j < 16; j++) {
+            stringstream ss;
+            ss << prefix.c_str() << j;
+            TH2* hh = ((TH2*)fileList.at(i)->Get(ss.str().c_str()));
+            hh->SetLineColor(hagCols.at(j));
+            hh->GetXaxis()->SetRangeUser(lowR, highR);
+            if (i == 0 && j == 0) {
+                hh->Draw("");
+            } else {
+                hh->Draw("same");
+            }
         }
     }
 }
