3e0d56b874034722aa070be7cbc2f2fed53ea9f3
T. T. King <tking.91@gmail.com>
2019-08-26 08:48:50 -0400

    working updates to the expQDCLoader

--- postGrad/ornl2016/expQDCLoad.cxx
+++ postGrad/ornl2016/expQDCLoad.cxx
@@ -1,23 +1,22 @@
 {
   TFile *_file0 = TFile::Open("New97Rb_test.root");
   gROOT->ProcessLine(".L ornlRoot-macros/NeutronCrayon.cxx+");
-  gROOT->ProcessLine(".L ornlRoot-macros/expQDCbackground.cxx+");
-  // TH2D* TvQ_h = SparseZP(dd_tof_h_qdc_tape,{3,{1,300}},{1,{1,-1}},{0,2},true);
+  //  gROOT->ProcessLine(".L ornlRoot-macros/expQDCbackground.cxx+");
+  // TH2D* TvQ_h = SparseZP(dd_tof_h_qdc_tape,{3,{1,300}},{1,{780,830}},{0,2},true)
+  ;
   // TvQ_h->SetName("TvQ_h");
   // expQDCbackground(TvQ_h,{400,600}, {20,20000},{20,140},132,{140,500},{500,2000},{2000,8000});
 
-  // TH2D* TvQ_c = SparseZP(dd_tof_c_qdc_tape,{3,{1,300}},{1,{1,-1}},{0,2},true);
+   // TH2D* TvQ_c = SparseZP(dd_tof_c_qdc_tape,{3,{1,300}},{1,{1,-1}},{0,2},true);
   // TvQ_c->SetName("TvQ_c");
-  // expQDCbackground(TvQ_c,{400,600}, {20,20000},{20,140},132,{140,500},{500,2000},{2000,8000});
+  // // expQDCbackground(TvQ_c,{400,600}, {20,20000},{20,140},132,{140,500},{500,2000},{2000,8000});
 
-  TH2D* TvQ_n = SparseZP(dd_tof_n_qdc_tape,{3,{1,300}},{1,{1,-1}},{0,2},true);
+   TH2D* TvQ_n = SparseZP(dd_tof_n_qdc_tape,{3,{1,300}},{1,{1,-1}},{0,2},true);
   TvQ_n->SetName("TvQ_n");
-  expQDCbackground(TvQ_n,{400,600}, {20,20000},{20,140},132,{140,500},{500,2000},{2000,8000});
+  //expQDCbackground(TvQ_n,{400,600}, {20,20000},{20,140},132,{140,500},{500,2000},{2000,8000});
+
 
 
   // expQDCbackground(dd_cortof_qdc,{400,600}, {20,20000},{20,140},132,{140,500},{500,2000},{2000,8000});
-
-
-
 
 }
